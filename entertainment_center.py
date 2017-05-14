851f05d1764d01ae38c698bf18be06e0d4fd27ca
sunrise2075 <sunrise2075@163.com>
2017-05-14 12:01:35 +0800

    modify file poster urls

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,13 +3,14 @@ import media
 
 toy_story = media.Movie("Toy Story",
 						"A story of a boy and his toys that come to life",
-						"https://en.wikipedia.org/wiki/File:Toy_Story.jpg",
+						"https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
 						"https://www.youtube.com/watch?v=OqIvPGT3oDQ")
 
 print toy_story.storyline
 
+
 avatar = media.Movie("Avatar","A marine on an alien planet",
-					 "https://en.wikipedia.org/wiki/File:Avatar-Teaser-Poster.jpg",
+					 "https://upload.wikimedia.org/wikipedia/zh/b/b0/Avatar-Teaser-Poster.jpg",
 					 "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 print avatar.storyline
