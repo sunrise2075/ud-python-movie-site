a24bdf749d99d655a32510b19588e36c9007df49
sunrise2075 <sunrise2075@163.com>
2017-05-16 01:42:55 +0800

    add new movie items

--- entertainment_center.py
+++ entertainment_center.py
@@ -21,14 +21,32 @@ transformer = media.Movie("Transformer",
 					 "https://upload.wikimedia.org/wikipedia/en/6/66/Transformers07.jpg",
 					 "https://www.youtube.com/watch?v=sgnO5fO46pE")
 
+citizenKane = media.Movie("Citizen Kane",
+	                 "Citizen Kane is a 1941 American mystery drama film by Orson Welles",
+					 "https://upload.wikimedia.org/wikipedia/en/c/ce/Citizenkane.jpg",
+					 "https://www.youtube.com/watch?v=8dxh3lwdOFw")
+
+twelveAngryMen = media.Movie("12 Angry men",
+	                 "12 Angry Men is a 1957 American courtroom drama film adapted from a teleplay of the same name by Reginald Rose",
+					 "https://upload.wikimedia.org/wikipedia/en/9/91/12_angry_men.jpg",
+					 "https://www.youtube.com/watch?v=gieIAysQTI0")
+
+toLive = media.Movie("To Live",
+	                 "A story about a Chinese family who is living on the yellow oil in China",
+					 "https://upload.wikimedia.org/wikipedia/zh/5/5c/%E7%94%B5%E5%BD%B1%E6%B4%BB%E7%9D%80.jpg",
+					 "https://www.youtube.com/watch?v=iEe0XwfbIl4")
+
 # print transformer.show_trailer()
 
-movie_list = [toy_story, avatar, transformer]
+movie_list = [
+	toy_story, avatar, transformer, citizenKane,
+	twelveAngryMen, toLive
+]
 
-# fresh_tomatoes.open_movies_page(movie_list)
+fresh_tomatoes.open_movies_page(movie_list)
 
-print media.Movie.__doc__
+# print media.Movie.__doc__
 
-print media.Movie.__name__
+# print media.Movie.__name__
 
-print media.Movie.__module__
+# print media.Movie.__module__
