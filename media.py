c61da6bed2ce434cef99b2ca33b30951d90bddad
sunrise2075 <sunrise2075@163.com>
2017-05-20 11:07:19 +0800

    add one line doc to definition of Movie class

--- media.py
+++ media.py
@@ -8,6 +8,14 @@ class Movie():
 	valid_ratings = ["G", "PG", "PG-13", "R"]
 
 	def __init__(self, movie_title, movie_storyline, poster_image_url, trailer_youtube_url):
+		"""
+			constructor of the python object which is currently under construction
+			@param self  the python object which is currently under construction
+			@param movie_title
+			@param movie_storyline
+			@param poster_image_url
+			@param trailer_youtube_url
+		"""
 		self.title = movie_title
 		self.storyline = movie_storyline
 		self.poster_image_url = poster_image_url
@@ -15,4 +23,7 @@ class Movie():
 
 
 	def show_trailer(self):
+		"""
+			open a browser window which is navigated to a youtub page of the movie trailer
+		"""
 		webbrowser.open(self.trailer_youtube_url)
